dedf2c20893ad2a315bf4ce4578f8956e4abe991
Mn0dsew <dima.nesterov.96@bk.ru>
2020-06-03 19:42:22 +0500

    add bomb spawner

---
+++ Assets/Scenes/BombDude/BombController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    System.Random rnd = new System.Random();
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        int force = rnd.Next(3, 15);
+        rb2D.AddForce(transform.right * -force, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/BombDude/BombSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawner : MonoBehaviour
+{
+
+    public GameObject BombPrefab;
+    public GameObject SpawnerParrent;
+    private float timer;
+    public float rateOfFire;
+    // Start is called before the first frame update
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0 && rateOfFire!=0)
+        {
+            var pos = SpawnerParrent.transform.position;
+            var rot = transform.rotation;
+            var Bomb = Instantiate(BombPrefab, pos, rot);
+            Bomb.transform.position = pos;
+            timer = rateOfFire;
+        }
+    }
+}
